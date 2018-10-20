710deffe28c568bb20550adbc7959d905c56ea2e
bry-an <bbyunis@gmail.com>
2018-10-20 14:48:06 -0600

    Serverside GraphQL working

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,24 @@
-const mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        //TODO: check if they are logged in
+        //async: ctx.db.mutation.createItem returns a promise, 
+        //and if want item to go into const item
 
-module.exports = mutations;
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info)
+        return item
+    }
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || []
+    //     //create a dog
+    //     const newDog = { name: args.name }
+    //     global.dogs.push(newDog)
+    //     return newDog
+    //     console.log(args)
+    // }
+};
+
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,15 @@
-const Query = {};
+// The returned data could come from anywhere, but usually from database
+//if you don't need anything special (auth, etc), can use
+//forwardTo, which will forward all info from Query in 
+//prisma.graphql to the below query function
+const { forwardTo } = require('prisma-binding')
+
+const Query = {
+    items: forwardTo('db'),
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items()
+    //     return items
+    // }
+};
 
 module.exports = Query;
