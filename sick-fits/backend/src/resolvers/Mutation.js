d8f6b3e564d99b4e40fe54d800d0049dce27773e
bry-an <bbyunis@gmail.com>
2018-10-21 07:38:13 -0600

    update item working

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -10,15 +10,30 @@ const Mutations = {
             }
         }, info)
         return item
+    },
+    updateItem(parent, args, ctx, info) {
+        //first take a copy of the updates
+        const updates = { ...args }
+        //remove ID from updates
+        delete updates.id
+        //run update method
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            }
+        }, info
+        )
+    },
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id }
+        //find the item
+        const item = await ctx.db.query.item({ where }, `{id title}`)
+        //check if they own that item, or have permish
+        //delete 
+        return ctx.db.mutation.deleteItem({ where }, info)
+
     }
-    // createDog(parent, args, ctx, info) {
-    //     global.dogs = global.dogs || []
-    //     //create a dog
-    //     const newDog = { name: args.name }
-    //     global.dogs.push(newDog)
-    //     return newDog
-    //     console.log(args)
-    // }
 };
 
 module.exports = Mutations;
