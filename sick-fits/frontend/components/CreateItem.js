25a70d2852992c2fc209e5bb6ace31a0c1ccb4b1
bry-an <bbyunis@gmail.com>
2018-10-21 12:35:58 -0600

    pagination/caching working

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -68,7 +68,8 @@ class CreateItem extends Component {
     }
     render() {
         return (
-            <Mutation mutation={CREATE_ITEM_MUTATION}
+            <Mutation 
+                mutation={CREATE_ITEM_MUTATION}
                 variables={this.state}>
                 {(createItem, { loading, error }) => (
                     <Form onSubmit={async (e) => {
