afb8f53f8deaf2ab69a6183c518698ff52ff2897
bry-an <bbyunis@gmail.com>
2018-10-23 06:34:01 -0600

    extract user from cookie

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,16 +1,29 @@
 // let's go!
 //entry point of app
 
+const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./createServer')
 const db = require('./db')
 
 
+
 const server = createServer()
 
 server.express.use(cookieParser())
 //TODO Use express middleware to  populate current user
+
+//decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+    const { token } = req.cookies
+    if (token) {
+        const { userId } = jwt.verify(token, process.env.APP_SECRET)
+        //put userID onto request for future requests to access
+        req.userId = userId
+    }
+    next()
+})
 
 
 server.start({
