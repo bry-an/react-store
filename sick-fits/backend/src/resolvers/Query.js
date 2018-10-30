6def8c87d0be2fd5cdb1c6f61364c371ee57e8c3
bry-an <bbyunis@gmail.com>
2018-10-29 21:26:22 -0600

    manage permissions skeleton

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,6 +3,10 @@
 //forwardTo, which will forward all info from Query in 
 //prisma.graphql to the below query function
 const { forwardTo } = require('prisma-binding')
+const { hasPermission } = require('../utils')
+
+
+//here, info includes the graphql query that contains the fields requested from the frontend
 
 const Query = {
     items: forwardTo('db'),
@@ -16,7 +20,20 @@ const Query = {
         return ctx.db.query.user({
             where: { id: ctx.request.userId }
         }, info)
+    },
+    async users(parent, args, ctx, info) {
+        //check if user is logged in 
+        if(!ctx.request.userId) {
+            throw new Error('You must be logged in to do this.')
+        }
+
+        //check if the user has the permissions to query the users
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
+
+        //if yes, query the users
+        return ctx.db.query.users({}, info)
     }
+
 };
 
 module.exports = Query;
