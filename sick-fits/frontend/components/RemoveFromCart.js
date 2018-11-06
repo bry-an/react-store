dbf46b18916198e1994dbc3f361986168069fbfc
bry-an <bbyunis@gmail.com>
2018-11-05 21:44:08 -0700

    optimistic response implementation

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -28,10 +28,32 @@ class RemoveFromCart extends React.Component {
     static propTypes = {
         id: PropTypes.string.isRequired
     }
+    // this gets called when we get response back from server after an update
+    update = (cache, payload) => {
+        // read the cache
+        const data = cache.readQuery({ query: CURRENT_USER_QUERY })
+        // remove that item from the cart
+        const cartItemId = payload.data.removeFromCart.id
+        data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId)
+        // write back to the cache
+        cache.writeQuery({ query: CURRENT_USER_QUERY, data })
+
+    }
     render() {
         return <Mutation 
             mutation = {REMOVE_FROM_CART_MUTATION}
-            variables = {{ id: this.props.id}}>
+            variables = {{ id: this.props.id}}
+            update={this.update}
+            optimisticResponse={{
+                __typename: 'Mutation', 
+                removeFromCart: {
+                    __typename: 'CartItem',
+                    id: this.props.id
+                }
+            }}
+            
+            >
+
         {(removeFromCart, { loading, error}) => (
         <BigButton
             title='Delete Item'
