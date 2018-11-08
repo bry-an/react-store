fa2e588217c1940dec34db704dccad1a0e5fc461
bry-an <bbyunis@gmail.com>
2018-11-07 21:43:31 -0700

    CartCount CSS

--- sick-fits/frontend/components/CartCount.js
+++ sick-fits/frontend/components/CartCount.js
@@ -1,23 +1,60 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import React from "react";
+import PropTypes from "prop-types";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
+const AnimationStyles = styled.span`
+    position: relative;
+    .count {
+        display: block;
+        position: relative;
+        transition: all 4s;
+        backface-visibility: hidden;
+    }
+    /* Initial state of the entered Dot will be flipped on its back */
+    .count-enter {
+        transform: rotateX(0.5turn);
+
+    }
+    .count-enter-active {
+        transform: rotateX(0);
+    }
+    .count-exit {
+        top: 0;
+        position: absolute;
+        transform: rotateX(0);
+    }
+    .count-exit-active {
+        transform: rotateX(0.5turn);
+    }
+`;
 
 const Dot = styled.div`
-    background: ${props => props.theme.red};
-    color: white;
-    border-radius: 50%;
-    padding: 0.5rem;
-    line-height: 2rem;
-    margin-left: 0.5rem;
-    min-width: 3rem;
-    font-weight: 100;
-    font-feature-settings:'tnum'; 
-    font-variant-numeric: tabular-nums
-
-`
+  background: ${props => props.theme.red};
+  color: white;
+  border-radius: 50%;
+  padding: 0.5rem;
+  line-height: 2rem;
+  margin-left: 0.5rem;
+  min-width: 3rem;
+  font-weight: 100;
+  font-feature-settings: "tnum";
+  font-variant-numeric: tabular-nums;
+`;
 
 const CartCount = ({ count }) => (
-    <Dot>{count}</Dot>
-)
+  <AnimationStyles>
+    <TransitionGroup>
+      <CSSTransition
+        unmountOnExit
+        className="count"
+        classNames="count"
+        key={count}
+        timeout={{ enter: 4000, exit: 4000 }}
+      >
+        <Dot>{count}</Dot>
+      </CSSTransition>
+    </TransitionGroup>
+  </AnimationStyles>
+);
 
-export default CartCount
+export default CartCount;
