f9e3fc1a5e20dfbeb9586982ecbb7087819c378c
bry-an <bbyunis@gmail.com>
2018-11-08 21:17:38 -0700

    removed item is also removed from cart

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -1,9 +1,8 @@
-
-import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import formatMoney from '../lib/formatMoney';
-import RemoveFromCart from './RemoveFromCart'
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import formatMoney from "../lib/formatMoney";
+import RemoveFromCart from "./RemoveFromCart";
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -20,25 +19,38 @@ const CartItemStyles = styled.li`
   }
 `;
 
-const CartItem = ({ cartItem }) => (
-  <CartItemStyles>
-    <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
-    <div className="cart-item-details">
-      <h3>{cartItem.item.title}</h3>
-      <p>
-        {formatMoney(cartItem.item.price * cartItem.quantity)}
-        {' - '}
-        <em>
-          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
-        </em>
-      </p>
-    </div>
-    <RemoveFromCart id={cartItem.id}/>
-  </CartItemStyles>
-);
+const CartItem = ({ cartItem }) => {
+  //check if item exists
+  if (!cartItem.item)
+    return (
+      <CartItemStyles>
+        <p>Item has been removed</p>
+        <RemoveFromCart id={cartItem.id} />
+      </CartItemStyles>
+    );
+
+  return (
+    <CartItemStyles>
+      {cartItem.item.image && (
+        <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
+      )}
+      <div className="cart-item-details">
+        <h3>{cartItem.item.title}</h3>
+        <p>
+          {formatMoney(cartItem.item.price * cartItem.quantity)}
+          {" - "}
+          <em>
+            {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
+          </em>
+        </p>
+      </div>
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+  );
+};
 
 CartItem.propTypes = {
-  cartItem: PropTypes.object.isRequired,
+  cartItem: PropTypes.object.isRequired
 };
 
 export default CartItem;
