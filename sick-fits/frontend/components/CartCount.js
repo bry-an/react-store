9e9a3cc87b8645acad7c537c97aa93f4adad7add
bry-an <bbyunis@gmail.com>
2018-11-08 20:46:27 -0700

    imported styled components to CardCount

--- sick-fits/frontend/components/CartCount.js
+++ sick-fits/frontend/components/CartCount.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
+import styled from 'styled-components'
 
 const AnimationStyles = styled.span`
     position: relative;
